469feccc5dfb59bf43eb8038792227f806122779
Evgeniy <ekudelya@intersog.com>
2021-05-09 17:05:24 +0300

    Added link for the repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,11 @@ export const Footer = () => {
       <div className='footer-copyright'>
         <div className='container'>
           © {new Date().getFullYear()} Copyright Text
-          <a className='grey-text text-lighten-4 right' href='#!'>
+          <a
+            className='grey-text text-lighten-4 right'
+            href='https://github.com/kudel89/react-shop'
+            target='_blank'
+            rel='noreferrer'>
             Repo
           </a>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,12 @@ export const Header = () => {
         </a>
         <ul id='nav-mobile' className='right hide-on-med-and-down'>
           <li>
-            <a href='#!'>Repo</a>
+            <a
+              href='https://github.com/kudel89/react-shop'
+              target='_blank'
+              rel='noreferrer'>
+              Repo
+            </a>
           </li>
         </ul>
       </div>
